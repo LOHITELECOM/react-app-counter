c6e64abd3c6897818cc95bb274a760b309ce9fb2
LOHITELECOM <s.lohidhasan071297@gmail.com>
2020-08-26 19:10:11 +0530

    1 file modified

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,11 +1,31 @@
 import React,{Component} from 'react';
 class Counter extends Component{
+  state={
+    count:0,
+    tags:['tag1','tag2','tag3']
+   
+  }
   render(){
+
     return(
       <div>
-      Hello
+      
+      <span className="badge badge-warning m-2">
+      {this.formateCount()} 
+      </span>
+      <button className="btn btn-secondary btn-sm" >Increment</button>
+      <ul>
+      {this.state.tags.map(tag=>
+      <li key={tag}>{tag}</li>)}
+      
+      </ul>
       </div>
     );
+  
   }
+    formateCount(){
+      const {count}=this.state
+      return count===0? 'Zero' :count
+    }
 }
 export default Counter;
