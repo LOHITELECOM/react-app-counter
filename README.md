5fb6095dab9b1bc5ea0bbc73f5eaa0a2b497d400
LOHITELECOM <s.lohidhasan071297@gmail.com>
2020-08-26 18:10:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-app-counter
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-app-counter)
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+import Counter from './Components/Counter';
+export default function App() {
+  return (
+    <div>
+      <Counter/>
+    </div>
+  );
+}
