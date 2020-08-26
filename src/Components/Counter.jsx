40968d50eaacd432e540e7da1e32f01335863f1a
LOHITELECOM <s.lohidhasan071297@gmail.com>
2020-08-26 19:23:18 +0530

    1 file modified

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -2,9 +2,12 @@ import React,{Component} from 'react';
 class Counter extends Component{
   state={
     count:0,
-    tags:['tag1','tag2','tag3']
-   
-  }
+    tags:[]
+   }
+   renderTags(){
+     if(this.state.tags.length===0) return <p>No tags found</p>;
+     return <ul>{this.state.tags.map(tag=><li key={tag}>{tag}</li>)} </ul>
+   }
   render(){
 
     return(
@@ -14,11 +17,8 @@ class Counter extends Component{
       {this.formateCount()} 
       </span>
       <button className="btn btn-secondary btn-sm" >Increment</button>
-      <ul>
-      {this.state.tags.map(tag=>
-      <li key={tag}>{tag}</li>)}
-      
-      </ul>
+     {this.state.tags.length===0 && "please enter tags"}
+     {this.renderTags()}
       </div>
     );
   
